2f4386eee1ad056849bb3fcfb3ac9c1e73216ff4
Lucas Marques <lucasmarquesds@gmail.com>
2017-08-13 17:53:19 -0300

    Add coverage and coveralls support

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, password=None):
         """
         Creates and saves a User with the given email, date of
         birth and password.
@@ -13,19 +13,19 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError('Users must have an email address')
 
-        user = self.model(email=self.normalize_email(email), username=username)
+        user = self.model(email=self.normalize_email(email))
 
         user.is_admin = False
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, email, password):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
-        user = self.create_user(email, username=username, password=password)
+        user = self.create_user(email, password=password)
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -41,8 +41,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         unique=True,
     )
 
-    username = models.CharField(_('Username'), max_length=50, unique=True)
-
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
@@ -52,7 +50,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     def get_full_name(self):
         # The user is identified by their email address
@@ -63,7 +60,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
     def __str__(self):
-        return self.username or self.email
+        return self.email
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
